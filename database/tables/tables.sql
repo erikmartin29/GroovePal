02fdd50c8546bb9faa238b2c9c6d10e80e4a0a37
anthonymolinari <molinaria@sonoma.edu>
2023-10-16 23:06:45 -0700

    update tables

--- database/tables/tables.sql
+++ database/tables/tables.sql
@@ -2,15 +2,18 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS secrets;
 
 CREATE TABLE users ( 
-    user_id VARCHAR(100) NOT NULL,
-    user_fname VARCHAR(40) NOT NULL,
-    user_lname VARCHAR(40) NOT NULL,
+    user_id     VARCHAR(100) NOT NULL,
+    user_pass   VARCHAR(100) NOT NULL,
+    user_fname  VARCHAR(40)  NOT NULL,
+    user_lname  VARCHAR(40)  NOT NULL,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE secrets (
-    secret_id VARCHAR(10) NOT NULL,
+    secret_id    VARCHAR(10)  NOT NULL,
     secret_value VARCHAR(255) NOT NULL,
+    owner_id     VARCHAR(100) NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(user_id),
     PRIMARY KEY (secret_id)
 );
 
