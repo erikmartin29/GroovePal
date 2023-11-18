7a22b5953cdcaeb727cfd94ba1096689bdd06e0d
ClaireAnderson98 <claire@Claires-Laptop.local>
2023-11-17 16:32:40 -0800

    basics of homepage done

--- ui/src/components/HomePage/UserHome.js
+++ ui/src/components/HomePage/UserHome.js
@@ -1,11 +1,76 @@
-import { Fragment } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Fragment, useState } from 'react';
+import { Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, List, ListItem, ListItemText } from '@mui/material';
+
+import testImage from './IMG_4254.jpeg';
+
+
+function createData (image, song, artist, album, timestamp) {
+    return {image, song, artist, album, timestamp};
+}
+
+const testRows = [
+      createData(testImage, "Test", "Nobody", "Neverland", "Today"),
+      createData(testImage, "Test", "Nobody", "Neverland", "Today"),
+      createData(testImage, "Test", "Nobody", "Neverland", "Today")
+];
+
+
+const RecentTracksRows = (props) => {
+    
+    const {row} = props
+    
+    return (
+            <TableRow key={row.song}>
+                <TableCell>
+                    <img src={row.image} width="100" height="100"/>
+                </TableCell>
+                <TableCell> {row.song} </TableCell>
+                <TableCell> {row.artist} </TableCell>
+                <TableCell> {row.album} </TableCell>
+                <TableCell> {row.timestamp} </TableCell>
+            </TableRow>
+    )
+}
+
+const TopListRows = (props) => {
+    
+    const {row, index, size} = props;
+    
+    let test = "test";
+    
+    if (index < size-1) {
+        
+        return (
+                <ListItem divider="true">
+                    <ListItemText primary="test"/>
+                </ListItem>
+                )
+    }
+    
+    return (
+            <ListItem>
+                <ListItemText primary={test} />
+            </ListItem>
+    )
+}
+
+const createList = (num) => {
+    return new Array(num).fill();
+}
 
 export default function UserHome() {
     
-    //Claire's Changes and Notes:
-    //added a user homepage that is more personalised for each user, needs ideas of what to display
+    const [recentTracks, setRecentTracks] = useState(createList(10));
+    const [topAlbums, setTopAlbums] = useState(createList(10));
+    const [topArtists, setTopArtists] = useState(createList(10));
     
+    /*
+     <TableCell>Thumbnail</TableCell>
+     <TableCell>Song</TableCell>
+     <TableCell>Artist</TableCell>
+     <TableCell>Album</TableCell>
+     <TableCell>Timestamp</TableCell>
+     */
     return (
             <Fragment>
                 <Box sx={{
@@ -13,24 +78,82 @@ export default function UserHome() {
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
-                    bgcolor: '#353939'
+                    bgcolor: '#353939',
+                    pt: 2,
+                    pb: 2
                 }}>
-                    <Typography sx={{
-                        color: 'white'
+                    <Box sx={{
+                        width: '70%',
+                        height: '90%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        pr: 2
                     }}>
-                    This is a personalised homepage for a user.
-                    <br />
-                    Ideas for this page:
-                    <br />
-                    -suggest more records based off of preferences (liked artists, vinyl albums of stuff listened to on lastFM, tags, etc)
-                    <br />
-                    -suggest records to buy/sell (info gained from Discogs somehow?)
-                    <br />
-                    -user statistics
-                    <br />
-                    - new note: if user hasn't linked their discogs or lastfm, show an alter here?
-                    <br />
-                    </Typography>
+                        <Typography variant="h4" sx={{
+                            color: 'white'
+                        }}>
+                            Recent Tracks
+                        </Typography>
+                        <TableContainer component={Paper}>
+                            <Table sx={{
+                                width: '100%',
+                                minHeight: '80vh'
+                            }}>
+                                <TableBody>
+                                    {
+                                        testRows.map((row) => (
+                                                               <RecentTracksRows row={row} />
+                                       ))
+                                    }
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Box>
+                    <Box sx={{
+                        width: '20%',
+                        height: '90%',
+                    }}>
+                        <Box sx={{
+                            mb: 2
+                        }}>
+                            <Typography variant="h4" sx={{
+                                color: 'white'
+                            }}>
+                                Top Albums
+                            </Typography>
+                            <List sx={{
+                                bgcolor: 'white',
+                                maxHeight: 300,
+                                position: 'relative',
+                                overflow: 'auto'
+                            }}>
+                                {
+                                    topAlbums.map((row, index) => (
+                                               <TopListRows row={row} index={index} size={topAlbums.length}/>
+                                        ))
+                                }
+                            </List>
+                        </Box>
+                        <Box>
+                            <Typography variant="h4" sx={{
+                                color: 'white'
+                            }}>
+                                Top Artists
+                            </Typography>
+                            <List sx={{
+                                bgcolor: 'white',
+                                maxHeight: 300,
+                                position: 'relative',
+                                overflow: 'auto'
+                            }}>
+                                {
+                                    topArtists.map((row, index) => (
+                                                                    <TopListRows row={row} index={index} size={topArtists.length}/>
+                                                                    ))
+                                }
+                            </List>
+                        </Box>
+                    </Box>
                 </Box>
             </Fragment>
     );
