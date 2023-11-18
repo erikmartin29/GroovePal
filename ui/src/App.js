ecf74e21411b2c9b87472c01276c25e50c411710
Erik Martin <techgeek1129@gmail.com>
2023-11-17 16:32:16 -0800

    theme improvements

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,16 @@
 import AppRouter from "./routing/AppRouter";
 import Header from "./components/Header/Header";
 import { AuthProvider } from "./context/AuthProvider";
+import { ThemeProvider } from "@mui/material";
 
 export default function App() {
     return (
         <AuthProvider>
-            <AppRouter>
-                <Header/>
-            </AppRouter>
+            
+                <AppRouter>
+                    <Header/>
+                </AppRouter>
+            
         </AuthProvider>
     );
 }
